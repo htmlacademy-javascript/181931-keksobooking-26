49af70219cf5feb3522e7c380eeb6935bcc93128
Keks <keks@htmlacademy.ru>
2022-06-06 15:11:01 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/test-data.js
@@ -0,0 +1,81 @@
+import {
+  getRandomPositiveFloatNumber,
+  getRandomPositiveNumber,
+  getRandomArrayElement,
+  getRandomArrayElements,
+  insertZeroBeforeDigit,
+  createArray,
+  getRandomNoRepeatValuesFromArray,
+} from './utils.js';
+
+const SIMILAR_PLACES_COUNT = 10;
+const MIN_MAX_NUMBERS = {
+  min: 1,
+  max: 100,
+};
+const IMAGE_NUMBERS = createArray(1, SIMILAR_PLACES_COUNT);
+const HOUSE_TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде',
+];
+const PLACE_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const PLACE_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const PLACE_CHECHIN = ['12:00', '13:00', '14:00'];
+const PLACE_CHECHOUT = ['12:00', '13:00', '14:00'];
+const PLACE_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTIONS = [
+  'Неуютное место за большие деньги',
+  ' Та еще конура',
+  'Это правда хорошее место',
+];
+
+const createPlace = () => {
+  const lat = getRandomPositiveFloatNumber(35.65, 35.7, 4);
+  const lng = getRandomPositiveFloatNumber(139.7, 139.8, 4);
+
+  const place = {
+    author: {
+      avatar: `img/avatars/user${insertZeroBeforeDigit(
+        getRandomNoRepeatValuesFromArray(IMAGE_NUMBERS, 1)[0]
+      )}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(HOUSE_TITLES),
+      address: `${lat.toString()} ${lng.toString()}`,
+      price: getRandomPositiveNumber(MIN_MAX_NUMBERS.min, MIN_MAX_NUMBERS.max),
+      type: getRandomArrayElement(PLACE_TYPES),
+      rooms: getRandomPositiveNumber(MIN_MAX_NUMBERS.min, MIN_MAX_NUMBERS.max),
+      guests: getRandomPositiveNumber(MIN_MAX_NUMBERS.min, MIN_MAX_NUMBERS.max),
+      checkin: getRandomArrayElement(PLACE_CHECHIN),
+      checkout: getRandomArrayElement(PLACE_CHECHOUT),
+      features: getRandomArrayElements(PLACE_FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElements(PLACE_PHOTOS),
+    },
+    location: {
+      lat: lat,
+      lng: lng,
+    },
+  };
+
+  return place;
+};
+
+export { createPlace, SIMILAR_PLACES_COUNT };
---
+++ 4/js/utils.js
@@ -0,0 +1,72 @@
+function getRandomPositiveNumber(min, max) {
+  if (min < 0) {
+    return 'Минимальное число должно быть больше или равно 0';
+  }
+  if (max <= min) {
+    return 'Максимальное число должно быть больше минимального';
+  }
+  min = Math.floor(min);
+  max = Math.round(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomPositiveFloatNumber(min, max, count) {
+  if (min < 0) {
+    return 'Минимальное число должно быть больше или равно 0';
+  }
+  if (max <= min) {
+    return 'Максимальное число должно быть больше минимального';
+  }
+  return parseFloat((Math.random() * (max - min) + min).toFixed(count));
+}
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomPositiveNumber(0, elements.length - 1)];
+
+const getRandomArrayElements = (elements) =>
+  elements.slice(0, getRandomPositiveNumber(0, elements.length));
+
+const insertZeroBeforeDigit = (digit) => {
+  if (String(digit).length === 1) {
+    return `0${digit}`;
+  }
+  return digit;
+};
+
+const createArray = (start, end) => {
+  const arr = [];
+
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+};
+
+const getRandomNoRepeatValuesFromArray = (source, quantity) => {
+  const randomValues = [];
+
+  if (source.length < quantity) {
+    return null;
+  }
+
+  for (let i = 0; i < quantity; i++) {
+    const elements = source.splice(
+      getRandomPositiveNumber(0, source.length - 1),
+      1
+    );
+    randomValues.push(elements[0]);
+  }
+
+  return randomValues;
+};
+
+export {
+  getRandomPositiveNumber,
+  getRandomPositiveFloatNumber,
+  getRandomArrayElement,
+  getRandomArrayElements,
+  insertZeroBeforeDigit,
+  createArray,
+  getRandomNoRepeatValuesFromArray,
+};
