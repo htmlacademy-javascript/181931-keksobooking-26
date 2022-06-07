bb7641771f93be74826880bf73a77ff50a3dfe44
alesukhin <alesukhin@museumplus.ru>
2022-06-07 14:11:26 +0300

    Отрисовывает карточку объявления

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-import { createPlace, SIMILAR_PLACES_COUNT } from './test-data.js';
-
-const similarPlaces = Array.from({ length: SIMILAR_PLACES_COUNT }, createPlace);
-
-console.log(similarPlaces); // Для теста :)
+import './create-card.js';
