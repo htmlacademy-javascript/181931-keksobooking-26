eb7810d6f9dc62709ae4276355a93a50702668f8
Keks <keks@htmlacademy.ru>
2022-07-20 14:07:54 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/api.js
@@ -0,0 +1,50 @@
+const ALERT_SHOW_TIME = 5000;
+
+const showError = (error) => {
+  const errorContainer = document.createElement('div');
+  errorContainer.style.zIndex = '100';
+  errorContainer.style.position = 'absolute';
+  errorContainer.style.left = '0';
+  errorContainer.style.top = '0';
+  errorContainer.style.right = '0';
+  errorContainer.style.padding = '10px 3px';
+  errorContainer.style.fontSize = '30px';
+  errorContainer.style.textAlign = 'center';
+  errorContainer.style.backgroundColor = 'tomato';
+  errorContainer.textContent = error;
+
+  document.body.append(errorContainer);
+
+  setTimeout(() => {
+    errorContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const getData = () =>
+  new Promise((resolve, reject) => {
+    fetch('https://26.javascript.pages.academy/keksobooking/data')
+      .then((response) => response.json())
+      .then((offers) => {
+        resolve(offers);
+      })
+      .catch(reject);
+  });
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch('https://26.javascript.pages.academy/keksobooking', {
+    method: 'POST',
+    body,
+  })
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      onFail('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+
+export { getData, sendData, showError };
---
+++ 12/js/main.js
@@ -0,0 +1,22 @@
+import { disableAdForm } from './form.js';
+import { enableAdForm } from './form.js';
+import { disableFilterForm, enableFilterForm } from './filters.js';
+import { enableMap } from './map.js';
+import { getData, showError } from './api.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  disableAdForm();
+  disableFilterForm();
+
+  const { setMarkerMoveHandler, resetMarker, setMarkers } = await enableMap();
+
+  enableAdForm(setMarkerMoveHandler, resetMarker);
+
+  try {
+    const offers = await getData();
+    setMarkers(offers);
+    enableFilterForm();
+  } catch (err) {
+    showError('Не удалось получить данные. Попробуйте ещё раз');
+  }
+});
